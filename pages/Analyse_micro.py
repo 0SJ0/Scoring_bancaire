2981a6627c944f611a4a1818c2a0d42414d01676
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-07-30 22:26:47 +0200

    Update Analyse_micro.py

--- pages/Analyse_micro.py
+++ pages/Analyse_micro.py
@@ -6,11 +6,18 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import plotly.graph_objects as go
 
-st.markdown("#  <center> :moneybag: Analyse macro :moneybag: </center> ", unsafe_allow_html=True)
+#Test
+df=pd.read_csv("Data/Test.csv")
+
+st.markdown("#  <h1 style='text-align: center; color: lightblue;'> :moneybag: Analyse micro :moneybag: </h1> ", unsafe_allow_html=True)
 
 st.sidebar.markdown("# 🎈 ANALYSE MICRO ")
 
 st.sidebar.markdown("Sur cette page, nous nous concentrons sur les caractéristiques individuelles de l'individu et les features locales qui jouent sur l'obtention ou non de son prêt.")
 
+liste_clients=list(df.SK_ID_CURR.values)
 
+ID_client = st.selectbox(
+     'Sélectionne un client :',
+     liste_clients)
 
