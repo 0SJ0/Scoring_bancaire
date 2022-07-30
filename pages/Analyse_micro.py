c70cb36750f3f5c7e8792885e52ad71b1cbef494
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-07-30 21:49:56 +0200

    Update Analyse_micro.py

--- pages/Analyse_micro.py
+++ pages/Analyse_micro.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import plotly.graph_objects as go
 
-st.markdown("# Page 2 ❄️")
+st.markdown("# <center> :moneybag: Analyse macro :moneybag: <\center>")
 
 
-st.write(":moneybag: Bonjour :moneybag:")
+
