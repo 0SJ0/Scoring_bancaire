8aa0304f01fba6093a6f4ee2c0b00af62cd52284
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-07-30 06:54:15 +0200

    Update page_3.py

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
+import plotly.graph_objects as go
+
 st.markdown("# Page 2 ❄️")
