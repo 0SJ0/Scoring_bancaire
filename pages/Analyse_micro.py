82ca50813a6b8defdbe494673b7c59abe0f225f5
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-07-30 22:23:03 +0200

    Update Analyse_micro.py

--- pages/Analyse_micro.py
+++ pages/Analyse_micro.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import plotly.graph_objects as go
 
-st.markdown("# <center> :moneybag: Analyse macro :moneybag: <\center>", unsafe_allow_html=True)
+st.markdown("#  <center> :moneybag: Analyse macro :moneybag: <\center> ", unsafe_allow_html=True)
 
 st.sidebar.markdown("# 🎈 ANALYSE MICRO ")
 
-st.sidebar.markdown("Dans cette page, nous nous concentrons sur les caractéristiques individuelles de l'individu et les features locales qui jouent sur l'obtention ou non de son prêt.")
+st.sidebar.markdown("Sur cette page, nous nous concentrons sur les caractéristiques individuelles de l'individu et les features locales qui jouent sur l'obtention ou non de son prêt.")
 
 
 
