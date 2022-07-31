ae93edd338a2f339e60798e3ae759f9c1ed87d0b
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-07-31 20:34:05 +0200

    Update Page_principale.py

--- Page_principale.py
+++ Page_principale.py
@@ -63,7 +63,7 @@ model= pd.read_pickle(r'Data/model.sav')
 explainer = shap.TreeExplainer(model[1])
 choosen_instance = df
 shap_values = explainer.shap_values(choosen_instance)
-st_shap(shap.summary_plot(shap_values,df,plot_size=[5*1.5,10*2])), height=700,width=1400)
+st_shap(shap.summary_plot(shap_values,df,plot_size=[5*2,10*2])), height=700,width=1400)
 
 
 choosen_instance = df.loc[21]
