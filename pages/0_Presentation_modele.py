85b67d63d47afb9b92dfd85f8b2a4bd5303835bf
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-18 11:22:47 +0200

    Update 0_Presentation_modele.py

--- pages/0_Presentation_modele.py
+++ pages/0_Presentation_modele.py
@@ -34,12 +34,9 @@ st.markdown("""
 
 
 
-with urllib2.urlopen("https://github.com/0SJ0/Scoring_bancaire/blob/main/Note_technique_scoring.pdf", "rb") as pdf_file:
-    base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
-    
-st.markdown(pdf_display, unsafe_allow_html=True)
-    
+image = Image.open('Note_technique_scoring_png.png')
+
+st.image(image, caption='Sunrise by the mountains')
    
 
 # Autres
