ee959fbfc2f08ed58bc6e72921d1c35e381ad84d
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-18 11:01:30 +0200

    Update 0_Presentation_modele.py

--- pages/0_Presentation_modele.py
+++ pages/0_Presentation_modele.py
@@ -30,6 +30,21 @@ st.markdown("""
 <embed src="Note_technique_scoring.pdf" width="800" height="800">
 """, unsafe_allow_html=True)
 
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+       
+
+    # Embedding PDF in HTML
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
+    
+    
+displayPDF("Note_technique_scoring.pdf")
+
 # Autres
 st.markdown("<h3 style='text-align: left; color: lightblue;'>Références</h3>", unsafe_allow_html=True)
 
