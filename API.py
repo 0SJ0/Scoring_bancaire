ac9a4da4aa7397f87efcaa0c7e304abad74ac465
Osjo <88280683+0SJ0@users.noreply.github.com>
2022-08-24 23:38:49 +0200

    Update API.py

--- API.py
+++ API.py
@@ -1,29 +1,46 @@
 # Import libraries
-import numpy as np
+import numpy 
 from flask import Flask, request, jsonify
+import gunicorn
+import fsspec
 import pickle
-import requests
+import imblearn
 import pandas as pd
-from flask import Flask, request
-import urllib.request as urllib2
+#import urllib.request as urllib2
+#import requests
+
+#S3 connexion
+#Téléchargement data plus modèle
+S3_connexion=0
 
 
-#Data
-
-#Chargement Modèle
-filename = 'DATA/model.sav'
-model = pickle.load(open(filename, 'rb'))
 
 
-#Chargement dataset
+    
+try :
+    #df = pd.read_csv("Data/data.csv",index_col=0).reset_index(drop=True)
+    df = pd.read_csv("Data/data.csv").reset_index(drop=True)
 
-target_url="https://scoring-credit.s3.eu-west-3.amazonaws.com/df_Xvalidation.txt"
-data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
-df = pd.read_csv(data, sep=',',index_col=0)
-df=df.reset_index(drop=True)
-df=df.iloc[:,0:]
+    S3_connexion+=1
+except :
+    print("Erreur")   
+    
+try :
+    model = pd.read_pickle("Data/model.sav.zip",compression='infer')
+    S3_connexion+=1
+except :
+    print("Erreur")
+    
+try :
+    model = pickle.load(open('Data/model.sav', 'rb')  )
+    S3_connexion+=1
+except :
+    print("Erreur")
+   
+    
+model = pd.read_pickle("Data/model.sav")
 
-
+    
 
 #Application
 app = Flask(__name__)
@@ -33,16 +50,23 @@ def hello_world():
     return 'Bienvenue sur le modèle de scoring'
 
 @app.route('/ID')
-def hello_worldo():
+def ID():
     return 'ID du client'
 
-@app.route('/ID/<ok>', methods=['GET'])
-def hello_worlda(ok):
-    ID=int(ok) #100194
-    index=df[df["SK_ID_CURR"]==ID].index.values[0]
-    proba=model.predict_proba(df.iloc[index:index+1,:])[0][1]
-    return jsonify({'proba' : proba})
-
+@app.route('/ID/<id>', methods=['GET'])
+def Prediction(id):
+    try :
+        ID=int(id) #100194
+        index=df[df["SK_ID_CURR"]==ID].index.values[0]
+        score=round(model.predict_proba(df.iloc[index:index+1,:])[0][1]*100) #368305
+        defaut_credit=0
+        if (score>30) : defaut_credit=1
+        return jsonify({'Score' : score, "Defaut_credit" : defaut_credit})
+    except :
+        return jsonify({"erreur" : S3_connexion})
+    
+    
+   
 
 
 if __name__ == '__main__':
